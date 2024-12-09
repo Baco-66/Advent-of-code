8dcc1893ea51731e0afb3ef7f16b1e37ebf1aee6
Luís Magalhães <luiscsousamagalhaes@gmail.com>
2024-12-09 16:14:35 +0000

    finished day 8

--- 2024/day_8/code.py
+++ 2024/day_8/code.py
@@ -1,84 +1,34 @@
-import itertools
-    
+from collections import defaultdict
+
 def parse_data_from_file(filename):
     """Reads data from a file and returns a list of lists"""
     with open(filename, 'r') as file:
         return [[char for char in line if char != "\n"] for line in file]
-    
+
 def print_matrix(matrix):
     for row in matrix:
         print(' '.join(map(str, row)))  # Convert each element to a string and join with spaces
 
-def find_positions_in_matrix(matrix, search):
-    return [[x, y] for x, line in enumerate(matrix) for y, value in enumerate(line) if value == search]
+def separate_by_frequency(values):
+    result = defaultdict(list)  # Create a dictionary to store lists
+    for a, x, y in values:
+        result[a].append((x, y))  # Group by 'a' and append (x, y)
+    return dict(result)  # Convert defaultdict to a regular dict for cleaner output
 
-def walk(map, size, position, direction):
-    nx, ny = position[0] + direction[0], position[1] + direction[1]
-    # Check bounds
-    if 0 <= nx < size[0] and 0 <= ny < size[1]: 
-        if map[nx][ny] == "#":  # Check if the target is blocked
-            return 0  # Cannot move to the new position
-        else:
-            position[0], position[1] = nx, ny  # Update position
-            return 1  # Move successful
-    else:
-        return 2  # Out of bounds
+def get_antenna_location(grid):
+    # Return all non-dot values as tuples of (value, x, y)
+    return [(value, int(x), int(y)) for x, row in enumerate(grid) for y, value in enumerate(row) if value != "."]
 
-def patrol(map, size, position):
-    # Create an infinite iterator
-    directions = itertools.cycle([
-        [-1, 0],  # Up
-        [0, 1],   # Right
-        [1, 0],   # Down
-        [0, -1],  # Left
-    ])
-    
-    for direction in directions:
-        # Continue walking while the step is valid (1) and stop if out of bounds (2)
-        while (step := walk(map, size, position, direction)) == 1:
-            map[position[0]][position[1]] = "X"  # Mark old position before attempting to walk
+def get_vector(point_a, point_b):
+    return (point_b[0] - point_a[0], point_b[1] - point_a[1])
 
-        # Stop patrolling if out of bounds
-        if step == 2:
-            break
-
-def find_loops(map, size, position, obstacle):
-    directions = itertools.cycle([
-        [-1, 0],  # Up
-        [0, 1],   # Right
-        [1, 0],   # Down
-        [0, -1],  # Left
-    ])
-    loop = set()
-    toutched = False 
-
-    for direction in directions:
-        # Continue walking while the step is valid (1) and stop if out of bounds (2)
-        while (step := walk(map, size, position, direction)) == 1:
-            pass
-        if step == 0:
-            if toutched:
-                magic = ((position[0], position[1], tuple(direction)))
-                if magic in loop :
-                    #print("Obstacle", obstacle, "caused the loop",loop)
-                    return True
-                loop.add(magic)
-
-            elif obstacle == [position[0] + direction[0], position[1] + direction[1]]:
-                magic = ((position[0], position[1], tuple(direction)))
-                loop.add(magic)
-                toutched = True
-
-        # Stop patrolling if out of bounds
-        elif step == 2:
-            return False
-
-    return False
+def add_vector(point, vector):
+    return (point[0] - vector[0], point[1] - vector[1])
 
 def main():
     # Example usage
     file_path = '2024/day_8/test_data.txt'
-    #file_path = '2024/day_8/data.txt' 
+    file_path = '2024/day_8/data.txt' 
 
     print("Advent of code day 8!")
     print("For part one type number 1")
@@ -87,13 +37,42 @@ def main():
     # Ask the user for the query to do
     query = input("What is the query?\n")
 
+    map = parse_data_from_file(file_path)
+    size = (len(map), len(map[0]))
+
+    antenas = separate_by_frequency(get_antenna_location(map))
+    antinodes = []
 
 
     match query:
         case "1":
-            pass
+            for locations in antenas.values():
+                for origin in locations:
+                    for location in locations:
+                        if origin != location:
+                            vector = get_vector(location, origin)
+                            antinode = add_vector(location, vector)
+                            if  0 <= antinode[0] < size[0] and 0 <= antinode[1] < size[1] and antinode not in antinodes:
+                                #map[antinode[0]][antinode[1]] = "#"
+                                #print("Vector",vector, "created from points", origin, location, "to generate", antinode)
+                                antinodes.append(antinode)
+                            #else:
+                            #    print("Vector",vector, "created from points", origin, location, "is out of bounds", antinode)
+            #print_matrix(map)
+            print(len(antinodes))
         case "2":
-            pass
+            for locations in antenas.values():
+                print(locations)
+                for origin in locations:
+                    for location in locations:
+                        if origin != location:
+                            vector = get_vector(location, origin)
+                            antinode = location
+                            while 0 <= antinode[0] < size[0] and 0 <= antinode[1] < size[1]:
+                                if antinode not in antinodes:
+                                    antinodes.append(antinode)
+                                antinode = add_vector(antinode, vector)
+            print(len(antinodes))
         case _:
             print("Invalid option!")
 
